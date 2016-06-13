9c4a9f7c75b6828ad48c72a8e0ba94b2d61f623f
ihorsiryi <for0work0@gmail.com>
2016-06-13 13:07:04 +0300

    fix composer.json

---
+++ src/Strkey/AbstractCRC.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Lib\Stellar\Strkey;
+
+abstract class AbstractCRC implements CRCInterface
+{
+    /**
+     *
+     * @var int
+     * @internal
+     */
+    public $checksum;
+    /**
+     *
+     * @var int
+     * @internal
+     */
+    protected $initChecksum = 0x0;
+    /**
+     *
+     * @var int
+     * @internal
+     */
+    public $xorMask = 0x0;
+    public function __construct()
+    {
+        $this->reset();
+    }
+    public function finish()
+    {
+        return $this->pack($this->getChecksum());
+    }
+    public function reset()
+    {
+        $this->checksum = $this->initChecksum;
+    }
+    /**
+     *
+     * @internal
+     */
+    public function getChecksum()
+    {
+        return $this->checksum ^ $this->xorMask;
+    }
+    /**
+     *
+     * @internal
+     */
+    abstract protected function pack($checksum);
+}
